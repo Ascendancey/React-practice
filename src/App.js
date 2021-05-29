5d7d875b73e29ea86c930f736cd6fc24f1ff2f92
Ascendancey <>
2021-05-30 00:23:15 +0300

    Created expense list, expense and date components. Wrapped into a card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Sweets",
+      amount: 55.34,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: "e2",
+      title: "Tuition fee",
+      amount: 149.0,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 500.0,
+      date: new Date(2021, 5, 14),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 140.56,
+      date: new Date(2021, 5, 1),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
