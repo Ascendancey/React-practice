f7585d4add12beb5523f923f004124e5c986e834
Ascendancey <mikhail.petukhov@yahoo.com>
2021-07-09 19:43:02 +0300

    List of exercises rendered from state. State defined in reducer and passed to app

--- src/components/Exercises/ExerciseList.js
+++ src/components/Exercises/ExerciseList.js
@@ -18,7 +18,20 @@ const ExerciseList = (props) => {
           <h4 className="exercises-list__headers__right">REPS</h4>
           <h4 className="exercises-list__headers__right">RPE</h4>
         </div>
-        <ExerciseItem
+        {count.map((item) => (
+          <ExerciseItem
+            exerciseid={item.exerciseid}
+            category={item.category}
+            title={item.title}
+            loadneed={item.loadneed}
+            loaddone={item.loaddone}
+            repsneed={item.repsneed}
+            repsdone={item.repsdone}
+            rpeneed={item.rpeneed}
+            rpedone={item.rpedone}
+          />
+        ))}
+        {/* <ExerciseItem
           exerciseid="0"
           category={"Push"}
           title={"1CT PAUSE BENCH PRESS"}
@@ -39,14 +52,16 @@ const ExerciseList = (props) => {
           repsdone={7}
           rpeneed={9}
           rpedone={10}
-        />
-        <label>{count}</label>
-        <button type="submit" onClick={() => alert("Submit placeholder")}>Submit</button>
+        /> */}
+        {/* <label>{count}</label> */}
+        <button type="submit" onClick={() => alert("Submit placeholder")}>
+          Submit
+        </button>
       </div>
     </Card>
   );
 };
 
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => state.counter.items;
 
 export default ExerciseList;
--- src/features/exercise/exerciseSlice.js
+++ src/features/exercise/exerciseSlice.js
@@ -2,44 +2,46 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
   name: "counter",
-  initialState: [
-    {
-      value: 0,
-      exerciseid: "0",
-      category: "Push",
-      title: "1CT PAUSE BENCH PRESS",
-      loadneed: 12,
-      loaddone: 15,
-      repsneed: 10,
-      repsdone: 5,
-      rpeneed: 8,
-      rpedone: 10,
-    },
-    {
-      value: 0,
-      exerciseid: "1",
-      category: "Push",
-      title: "1CT PAUSE BENCH PRESS",
-      loadneed: 2,
-      loaddone: 5,
-      repsneed: 0,
-      repsdone: 15,
-      rpeneed: 18,
-      rpedone: 1,
-    },
-  ],
+  initialState: {
+    items: [
+      {
+        value: 0,
+        exerciseid: "0",
+        category: "Push",
+        title: "1CT PAUSE BENCH PRESS",
+        loadneed: 12,
+        loaddone: 15,
+        repsneed: 10,
+        repsdone: 5,
+        rpeneed: 8,
+        rpedone: 10,
+      },
+      {
+        value: 0,
+        exerciseid: "1",
+        category: "Push",
+        title: "1CT PAUSE BENCH PRESS",
+        loadneed: 2,
+        loaddone: 5,
+        repsneed: 0,
+        repsdone: 15,
+        rpeneed: 18,
+        rpedone: 1,
+      },
+    ],
+  },
   reducers: {
     updateValue(state, action) {
       const newItem = action.payload;
-      const existingItem = state.find((item) => item.exerciseid === newItem.exerciseid);
-      const updType = newItem.type;
-      existingItem[updType + 'done'] = Number(action.payload.event.target.value);
+      const existingItem = state.items.find(
+        (item) => item.exerciseid === newItem.exerciseid
+      );
+      const updType = newItem.type + "done";
+      existingItem[updType] = Number(action.payload.event.target.value);
     },
   },
 });
 
-export const {
-  updateValue,
-} = counterSlice.actions;
+export const { updateValue } = counterSlice.actions;
 
 export default counterSlice.reducer;
